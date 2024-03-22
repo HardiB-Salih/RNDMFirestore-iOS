d879e01969d6d2a0c2c665af653b0e927822020b
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2024-03-22 11:16:16 +0300

    first commit

---
+++ RNDMFirestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RNDMFirestore
+//
+//  Created by HardiB.Salih on 3/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
+    }
+
+
+}
+
